e2f072ddd6f5ed7e561d8b7ef1c5be53b56e7e70
gmsolem <geir.solem.m@gmail.com>
2021-09-01 13:03:11 -0500

    Update BIBTtoken.sol

--- BIBTtoken.sol
+++ BIBTtoken.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
-// BIBT-erc20-token
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract BIBTtoken is ERC20 {
-    constructor() ERC20("BIBTtoken", "BIBT") {
+contract BIBTercxx is ERC20 {
+    constructor() ERC20("BIBTercxx", "BIBT") {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 }
